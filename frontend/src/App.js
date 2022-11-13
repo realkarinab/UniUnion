922a2056a9dc76eb2383c3b91dd6b99ac096f2cd
Karina Batra <realkarinab@gmail.com>
2022-11-12 21:45:01 -0600

    pushing to 55:39

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React, { useEffect } from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import Login from './components/login';
+import Home from './container/Home'
+
+export default function App() {
+    return (
+    <Routes>
+        <Route path="login" element={<Login />} />
+        <Route path="/*" element={<Home />} />
+      </Routes>
+    )
+  }
+
